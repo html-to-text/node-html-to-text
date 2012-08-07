6694d7e44239f007124e4f671508a937676ad490
Malte Legenhausen <mlegenhausen@gmail.com>
2012-08-07 13:27:36 +0200

    State machine added

--- test/html-to-text.js
+++ test/html-to-text.js
@@ -2,7 +2,9 @@ var path = require('path');
 
 var htmlToText = require('../lib/html-to-text');
 
-htmlToText.fromFile(path.join(__dirname, 'test.html'), function(err, text) {
+htmlToText.fromFile(path.join(__dirname, 'test.html'), {
+	tables: ['invoice', 'address']
+}, function(err, text) {
 	if (err) return console.error(err);
 	console.log(text);
 });
