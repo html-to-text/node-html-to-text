e8d6ee5f6b39372389400e87f077df54d6e8c36d
Malte Legenhausen <mlegenhausen@gmail.com>
2012-08-08 17:00:12 +0200

    Body is now optional. Version bump

--- example/html-to-text.js
+++ example/html-to-text.js
@@ -2,6 +2,14 @@ var path = require('path');
 
 var htmlToText = require('../lib/html-to-text');
 
+console.log('fromString:')
+var text = htmlToText.fromString('<h1>Hello World</h1>', {
+	wordwrap: 130
+});
+console.log(text);
+console.log();
+
+console.log('fromFile:');
 htmlToText.fromFile(path.join(__dirname, 'test.html'), {
 	tables: ['invoice', 'address']
 }, function(err, text) {
