7dda2eb63fe6c56dee92ea6c034efd67a96c814a
Ivan Pantic <ivanpantic82@gmail.com>
2014-11-12 11:08:47 +0100

    Added support for custom handler

--- example/html-to-text.js
+++ example/html-to-text.js
@@ -2,17 +2,39 @@ var path = require('path');
 
 var htmlToText = require('../lib/html-to-text');
 
-console.log('fromString:')
+console.log('fromString:');
 var text = htmlToText.fromString('<h1>Hello World</h1>', {
 	wordwrap: 130
 });
 console.log(text);
 console.log();
 
-console.log('fromFile:');
 htmlToText.fromFile(path.join(__dirname, 'test.html'), {
 	tables: ['#invoice', '.address']
 }, function(err, text) {
 	if (err) return console.error(err);
+	console.log('fromFile:');
 	console.log(text);
+	console.log();
 });
+
+htmlToText.fromFile(
+	path.join(__dirname, 'test-custom.html'),
+	{
+		tables: ['#tabular_data'],
+		custom: function (elem) {
+			if (elem.type === 'tag' && elem.name === 'img' && elem.attribs['alt'] === 'graph') {
+				return "To see the graph, visit here: http://my-site.com/reports/graph";
+			}
+			if (elem.type === 'tag' && elem.name === 'table' && elem.attribs['id'] === 'tabular_data') {
+				return ["(For best results, please see the HTML version, or visit our site)\n", true];
+			}
+		}
+	},
+	function(err, text) {
+		if (err) return console.error(err);
+		console.log('custom:');
+		console.log(text);
+		console.log();
+	}
+);
