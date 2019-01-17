751518c9b30ee304f8105ac33e9af316b7720bc1
hmudassir <muhammad.mudassir@techtronix.bix>
2019-01-17 14:42:24 +0500

    added merged tags feature

--- example/html-to-text.js
+++ example/html-to-text.js
@@ -4,14 +4,24 @@ var htmlToText = require('../lib/html-to-text');
 
 console.log('fromString:');
 var text = htmlToText.fromString('<h1>Hello World</h1>', {
-  wordwrap: 130
+  wordwrap: 130,
+  hasMergedTags: true,
+  mergedTags: {
+    'name': 'Mudassir'
+  }
 });
 console.log(text);
 console.log();
 
 console.log('fromFile:');
 htmlToText.fromFile(path.join(__dirname, 'test.html'), {
-  tables: ['#invoice', '.address']
+  tables: ['#invoice', '.address'],
+  hasMergedTags: true,
+  mergedTags: {
+    'name': 'Mudassir',
+    'email': 'mudassir@test.com',
+    'date': '12 Feb 2019'
+  }
 }, function(err, text) {
   if (err) return console.error(err);
   console.log(text);
