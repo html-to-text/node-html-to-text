07faa96589f7d0f857b92fd8c2914b4ef54ff62b
KillyMXI <killy.mxi@gmail.com>
2022-08-17 21:45:12 +0400

    Enable ESM and CJS output from Rollup

--- packages/html-to-md/rollup.config.js
+++ packages/html-to-md/rollup.config.js
@@ -6,7 +6,8 @@ const cjs = require('rollup-plugin-cjs-es');
 module.exports = {
   input: 'src/html-to-md.js',
   output: [
-    { file: 'lib/html-to-md.js', format: 'cjs' }
+    { file: 'lib/html-to-md.mjs', format: 'es' },
+    { file: 'lib/html-to-md.cjs', format: 'cjs' }
   ],
   plugins: [
     nodeResolve({ resolveOnly: ['@html-to-text/base'] }),
--- packages/html-to-text/rollup.config.js
+++ packages/html-to-text/rollup.config.js
@@ -6,7 +6,8 @@ const cjs = require('rollup-plugin-cjs-es');
 module.exports = {
   input: 'src/html-to-text.js',
   output: [
-    { file: 'lib/html-to-text.js', format: 'cjs' }
+    { file: 'lib/html-to-text.mjs', format: 'es' },
+    { file: 'lib/html-to-text.cjs', format: 'cjs' },
   ],
   plugins: [
     nodeResolve({ resolveOnly: ['@html-to-text/base'] }),
